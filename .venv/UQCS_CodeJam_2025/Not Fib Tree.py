69ded4ceafb16102c53022e552fdb72ef0aec4f6
Borca <148187221+BorcaWill@users.noreply.github.com>
2025-05-04 01:06:30 +1000

    Using DP to crack another Fib problem

--- .venv/UQCS_CodeJam_2025/Not Fib Tree.py
+++ .venv/UQCS_CodeJam_2025/Not Fib Tree.py
@@ -19,39 +19,24 @@ def countFibonacciNumbers(count, nums):
     # Write your code here
     result = 0
     for i in range(count):
-        if isFibonacci(nums[i]):
+        if nums[i] in fiboArray(100):    
             result += 1
     return result
 
-def isFibonacci(num):
-    # Check if num is a Fibonacci number
-    if num in [1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946]:
-        return True
-    elif num> 10946:
-        i = 20
-        while True:
-            if fibonacciLeaves(i) == num:
-                return True
-            elif fibonacciLeaves(i) < num:
-                i += 1
-            else:
-                return False
+def fiboArray(n):
+    memo = (n+1) * [-1]
+    for i in range(n+1):
+        memo[i] = fibRecursion(i,memo)
+    return memo
 
-    
-def fibonacciLeaves(num):
-    # Write your code here
-    if num == 0 or num == 1:
-        return 1
-    elif num == 5:
-        return 8
-    elif num == 10:
-        return 89
-    elif num == 20:
-        return 10946
-    elif num > 1:
-        return fibonacciLeaves(num - 1) + fibonacciLeaves(num - 2)
-    else: 
-        raise ValueError("Input must be a non-negative integer")
+def fibRecursion(n,memo):
+    if n == 0 or n == 1:
+        return n
+    if memo[n] != -1:
+        return memo[n]
+    memo[n] = fibRecursion(n - 1,memo) + fibRecursion(n - 2,memo)
+    return memo[n]
+
     
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
