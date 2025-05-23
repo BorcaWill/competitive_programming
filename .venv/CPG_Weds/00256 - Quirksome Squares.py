56dc53164ed6567d2639ddddd068b20bbb0aac77
Borca <148187221+BorcaWill@users.noreply.github.com>
2025-05-23 22:36:19 +1000

    use DP to do quirksome_squares

--- .venv/CPG_Weds/00256 - Quirksome Squares.py
+++ .venv/CPG_Weds/00256 - Quirksome Squares.py
@@ -1,5 +1,5 @@
 import sys
-
+dict_of_quirksome_squares = dict()
 def main():
     input = sys.stdin.read
     data = input().strip().split("\n")
@@ -8,13 +8,20 @@ def main():
     for i in range(10**(max(data)//2)):
         square_numbers.append(i**2)
     for l in data:
-        for i in square_numbers:
-            if i>=10**l:
-                break
-            first_half = i // (10**(l//2))
-            second_half = i % (10**(l//2))
-            if (first_half + second_half)**2 == i:
+        if l in dict_of_quirksome_squares:
+            for i in dict_of_quirksome_squares[l]:
                 print(f"{i:0{l}d}")
+        else:
+            for i in square_numbers:
+                if i>=10**l:
+                    break
+                first_half = i // (10**(l//2))
+                second_half = i % (10**(l//2))
+                if (first_half + second_half)**2 == i:
+                    if l not in dict_of_quirksome_squares:
+                        dict_of_quirksome_squares[l] = list()
+                    dict_of_quirksome_squares[l].append(i)
+                    print(f"{i:0{l}d}")
 
 if __name__ == "__main__":
     main()
